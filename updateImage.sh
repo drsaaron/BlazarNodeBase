6b57f00913873c19c4b04b96dbdd2acd045178fd
Scott Aaron <dr_saaron@yahoo.com>
2022-03-25 13:01:09 -0500

    fix the sort

--- updateImage.sh
+++ updateImage.sh
@@ -16,7 +16,7 @@ imageName=$(dockerImageName.sh)
 
 if pullLatestDocker.sh || [ -n "$force" ]
 then
-    currentVersion=$(docker images $imageName | sed 1d | awk '$2 !~ /latest/ { print $2 }' | sort -r | head -1)
+    currentVersion=$(docker images $imageName | sed 1d | awk '$2 !~ /latest/ { print $2 }' | sort | head -1)
     nextVersion=$(echo $currentVersion |awk -F\. '{printf "%s.%s", $1, $2+1}')
     echo "docker base updated, so refreshing uses to version $nextVersion (from $currentVersion)"
     buildImage.sh -v $nextVersion
